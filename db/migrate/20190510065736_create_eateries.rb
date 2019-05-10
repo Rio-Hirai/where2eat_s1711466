0d4fdf028f4e6825d899e9d41d568df1c24c51c3
Rio Hirai <s1711466@s.tsukuba.ac.jp>
2019-05-10 16:03:30 +0900

    ルートパスを設定

---
+++ db/migrate/20190510065736_create_eateries.rb
@@ -0,0 +1,13 @@
+class CreateEateries < ActiveRecord::Migration[5.2]
+  def change
+    create_table :eateries do |t|
+      t.string :name
+      t.string :zip
+      t.string :address
+      t.string :tel
+      t.string :url
+
+      t.timestamps
+    end
+  end
+end
